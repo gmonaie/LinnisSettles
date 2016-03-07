6e5df72c2fb45afbbdad0c3b729d1e7ce79e0919
Gei Lin <gmonaie@gmail.com>
2016-03-07 23:08:18 +0000

    capture dryRun as the last commandline argument

--- settles-task/settles_task.R
+++ settles-task/settles_task.R
@@ -1,4 +1,8 @@
 suppressMessages(library(LinnisSettles))
+args <- commandArgs(trailingOnly = TRUE)
+dryRun <- as.logical(last(args))
+
+# grab the last args parameter and set that to dryRun
 
 dwidth <- getOption("width")
 options(width = 1000)
